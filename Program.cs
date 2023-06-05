917fe840ec16cebc5fd8e085e5b6f435d10c1ad4
mohammed3599 <kataloni1999@gmail.com>
2023-06-05 11:48:09 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Nested_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********");
+            /*Print the following pattern
+             
+             *
+             **
+             ***
+             ****
+             *****
+             ******
+             *******
+             ********
+             *********
+             **********
+             
+                             */
+            for(int i = 1; i <= 10; i++)
+            {
+                for(int j = 0; j < i;  j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            /*Print the following pattern
+             *********
+             ********
+             *******
+             ******
+             *****
+             ****
+             ***
+             **
+             *
+             
+                   */
+            for (int i = 9; i >= 1; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("**********");
+
+        }
+    }
+}
